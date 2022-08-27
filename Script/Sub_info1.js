cf35211f417511e05073ec7e19c6ccb9d36c69ed
YTOO <73226350+hiLGis@users.noreply.github.com>
2022-08-27 23:38:22 +0800

    Update Sub_info1.js

--- Script/Sub_info1.js
+++ Script/Sub_info1.js
@@ -58,7 +58,7 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} ` + `| 𝗥𝗲𝘀𝗲𝘁 : ` + `${resetDayLeft} d`,
+    title: `${args.title} ` + `| 𝗥𝗲𝘀𝗲𝘁 : ` + `${resetDayLeft} 𝗱𝗮𝘆`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
