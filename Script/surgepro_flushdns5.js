8d154b52c5288540da64066fa12b339122390c92
YTOO <73226350+hiLGis@users.noreply.github.com>
2022-11-24 22:28:52 +0800

    Update surgepro_flushdns5.js

--- Script/surgepro_flushdns5.js
+++ Script/surgepro_flushdns5.js
@@ -11,7 +11,7 @@ if ($trigger == "button") await httpAPI("/v1/dns/flush");
 
   $done({
       title:"𝗦𝗨𝗥𝗚𝗘 𝗣𝗥𝗢 ®",
-      content:`启动时长：${startTime}`,
+      content:`启动时长：${startTime}\n`,
 		icon: params.icon,
 		"icon-color":params.color
     });
