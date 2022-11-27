207bc0e1bb57d50a97038dd90eae77705c28b22b
ATO <73226350+hiLGis@users.noreply.github.com>
2022-11-28 04:29:55 +0800

    Update Sub_info1.js

--- Script/Sub_info1.js
+++ Script/Sub_info1.js
@@ -58,7 +58,7 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} ` + `| 𝕣𝕖𝕤𝕖𝕥 ➟ ` + `${resetDayLeft} 🅳`,
+    title: `${args.title} ` + `| 𝕣𝕖𝕤𝕖𝕥 ➟ ` + `${resetDayLeft}日`,
     content: content.join("\n"),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
