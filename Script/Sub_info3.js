710d7b77bfa8856d6a548b5bc7a80117bf8f9b3a
ATO <73226350+hiLGis@users.noreply.github.com>
2022-11-27 12:27:45 +0800

    Update Sub_info3.js

--- Script/Sub_info3.js
+++ Script/Sub_info3.js
@@ -35,12 +35,12 @@ let args = getArgs();
   if (resetDayLeft) {
     content.push(`重置：剩余${resetDayLeft}天`);
   }
-*/
+
   if (expire) {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
     content.push(`𝗗𝗮𝘁𝗲：${formatTime(expire)}`);
   }
-
+*/
   let now = new Date();
   let hour = now.getHours();
   let minutes = now.getMinutes();
@@ -48,7 +48,7 @@ let args = getArgs();
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} ` + `|icon:` + `args.icon||"airplane.circle"`,
+    title: `${args.title} ` + `|𝗥𝗲𝘀𝗲𝘁 : ` + `${expire} 𝗱𝗮𝘆`,
     content: content.join("\n"),
     "icon-color": args.color || "#007aff",
   });
